7f459be0c0a68bdf718c74e65a1b946f162e252f
Alexander Hoinville <ahoin001@gmail.com>
2019-08-05 12:40:57 -0400

    Complete front end (iteration 1)

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,14 +6,23 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
+
+// directories we will use views from 
 app.set('views', __dirname + '/views');
+
+// directory we will use static files from (images,css)
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/Home', (req, res, next) => {
+  res.render('home');
 });
 
 
